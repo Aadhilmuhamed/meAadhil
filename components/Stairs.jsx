7730f5f6df3a1125d6a288ece5e7b48f7f195e7a
aadhilmuhamed2022 <aadhilmuhamed2022@gmail.com>
2024-07-20 12:53:24 +0000

    my-start-homepage

---
+++ components/Stairs.jsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+import { exit } from "process";
+import React from "react";
+
+const stairanimation = {
+  initial: {
+    bottom: "0%",
+  },
+  animate: {
+    bottom: "100%",
+  },
+  exit: {
+    bottom: ["100%", "0%"],
+  },
+};
+
+const reverseIndex = (index) => {
+  const totalSteps = 10;
+  return totalSteps - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+      {/* rendder 8motion divs */}
+      {[...Array(10)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairanimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.8,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-[#bbf7d0] relative"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Stairs;
