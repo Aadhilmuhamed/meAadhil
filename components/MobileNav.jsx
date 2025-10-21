7cd16c42c8b958952c58868d2b079be694a57353
Aadhil Mohamed <131958833+Aadhilmuhamed@users.noreply.github.com>
2025-10-21 21:35:59 +0530

    lint_fix

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,12 +1,12 @@
 "use client";
-import React from 'react';
-import {Sheet,SheetContent ,SheetTrigger} from "@/components/ui/sheet"
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { CiMenuFries } from "react-icons/ci";
 
-const links =[
+const links = [
   {
     name: "Home",
     path: "/",
@@ -27,29 +27,21 @@ const links =[
     name: "Contact",
     path: "/contact",
   },
-]
+];
 
 const MobileNav = () => {
-const pathname =  usePathname()
-
-
-
+  const pathname = usePathname();
 
   return (
-    <div >
-  <Sheet>
-    <SheetTrigger className='flex justify-center items-center gap-4'>
-<CiMenuFries className="text-{32px} text-accent "/>
-
-    </SheetTrigger>
-    <SheetContent className="flex">
-        <div>Logo</div>
-    </SheetContent>
-
-
-  </Sheet>
-
-   
+    <div>
+      <Sheet>
+        <SheetTrigger className="flex justify-center items-center gap-4">
+          <CiMenuFries className="text-{32px} text-accent " />
+        </SheetTrigger>
+        <SheetContent className="flex">
+          <div>Logo</div>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
--- components/stairEffect.jsx
+++ components/stairEffect.jsx
@@ -13,17 +13,17 @@ const StairEffect = () => {
         <div key={pathname}>
           <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
             <Stairs />
-          <motion.div
-            className="h-screen w-screen fixed bg=primary top-0 pointer-events-none "
-            inital={{ opacity: 1 }}
-            animate={{ opacity: 0 }}
-            transition={{
-              delay: 1,
-              duration: 0.4,
-              ease: "easeInOut",
-            }}
-          ></motion.div>
-           </div>
+            <motion.div
+              className="h-screen w-screen fixed bg=primary top-0 pointer-events-none "
+              inital={{ opacity: 1 }}
+              animate={{ opacity: 0 }}
+              transition={{
+                delay: 1,
+                duration: 0.4,
+                ease: "easeInOut",
+              }}
+            ></motion.div>
+          </div>
         </div>
       </AnimatePresence>
     </>
