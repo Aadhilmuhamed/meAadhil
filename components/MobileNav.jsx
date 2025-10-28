4436b744e77d487e401b7f7a9002568543c2ea99
aadhilmuhamed2022 <aadhilmuhamed2022@gmail.com>
2025-10-28 18:19:37 +0000

    add_bug

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,12 +1,12 @@
 "use client";
-import React from 'react';
-import {Sheet,SheetContent ,SheetTrigger} from "@/components/ui/sheet"
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { CiMenuFries } from "react-icons/ci";
 
-const links =[
+const links = [
   {
     name: "Home",
     path: "/",
@@ -27,30 +27,34 @@ const links =[
     name: "Contact",
     path: "/contact",
   },
-]
+];
 
 const MobileNav = () => {
-const pathname =  usePathname()
-
-
-
+  const pathname = usePathname();
 
   return (
-    <div >
-  <Sheet>
-    <SheetTrigger className='flex justify-center items-center gap-4'>
-<CiMenuFries className="text-{32px} text-accent "/>
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center gap-4">
+        <CiMenuFries className="text-{32px} text-accent " />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* liogo */}
+        <div className="mt-32 mb-16 text-center text-white text-3xl ">
+          <Link href="/">
+            <h1 className="font-semiibold text-4xl">
+              ADL<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
 
-    </SheetTrigger>
-    <SheetContent className="flex">
-        <div>Logo</div>
-    </SheetContent>
-
-
-  </Sheet>
-
-   
-    </div>
+        <nav>
+          {links.map((l, i) => {
+            return <Link href={l.path} key={i} className="text-3xl flex flex-col mb-8 text-center" >{l.name}</Link>;
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
   );
 };
 
