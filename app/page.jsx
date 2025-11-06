57ad77b54ff29ef2335324552521cc2eb47083fa
Aadhil Mohamed <131958833+Aadhilmuhamed@users.noreply.github.com>
2025-11-06 22:45:38 +0530

    homepage_add

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
 const Page = () => {
-  return <div>Homepage</div>;
+  return (
+    <section className="h-full">
+      <div className="container h-full mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb:24">
+          <div className="text=center xl:text-left">
+            <span className="text-xl">Softwere developer</span>
+            <h1 className="h1 mb-6">
+              Hello I&apos;m <br />
+              <span className="text-accent">Aadhil</span>
+            </h1>
+          </div>
+          <div className="max-w-[500px] mb-9 text-white/80">
+            Software Engineer with a focus on front-end development and
+            expertise in the React and Next.js ecosystem.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
